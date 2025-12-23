40005b71ed1eec77f06a60007c395b28db9830d7
kotani6053 <yoshioka@e-kotani.co.jp>
2025-12-23 09:51:21 +0900

    Update Collection.jsx

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -1,1 +1,15 @@
+import React from 'react';
+
+function Collection({ cards }) {
+  return (
+    <div>
+      <h3>コレクション</h3>
+      {cards.map((c, i) => (
+        <div key={i} className={`card ${c.rarity}`}>{c.name}</div>
+      ))}
+    </div>
+  );
+}
+
+export default Collection;
 
